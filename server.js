1621a33ea515f6b323167e3a74d394d3dec2f5b8
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2023-05-21 13:52:15 +0700

    apply swagger for project

--- server.js
+++ server.js
@@ -6,12 +6,58 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 const route = require('./src/routes');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
+
+// set up Swagger
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Spending Management API',
+    summary: 'Provide a set of API for Spending Management Application',
+    version: '1.0.1',
+    license: {},
+    contact: {
+      name: 'Maintenance team',
+      url: '',
+    },
+  },
+  servers: [
+    {
+      url: `http://localhost:{port}`,
+      variables: {
+        port: {
+          default: PORT,
+        },
+      },
+    },
+  ],
+};
+const options = {
+  swaggerDefinition,
+  apis: [
+    path.join(__dirname, './src', '/api_docs', '*.yaml'),
+    path.join(__dirname, './src', '/api_docs', '*', '*.yaml'),
+  ],
+  exflore: true,
+};
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 route(app);
 
@@ -25,8 +71,6 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`);
