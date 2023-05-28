3c576447eba8c44ecb8d3d3a4a5f6c323c48ce60
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2023-05-28 22:25:55 +0700

    delete test api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 const authRouter = require('./auth');
-const testRouter = require('./test');
 const userRouter = require('./user.route');
 const invoiceIntoStockRouter = require('./invoiceIntoStock.route');
 const categoryRouter = require('./category.route');
@@ -9,7 +8,6 @@ const productRouter = require('./product.route');
 
 function route(app) {
   app.use('/api/auth', authRouter);
-  app.use('/api/test', testRouter);
   app.use('/api/users', userRouter);
   app.use('/api/invoice-into-stocks', invoiceIntoStockRouter);
   app.use('/api/categories', categoryRouter);
--- src/routes/test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const testControllers = require('../controllers/test.controller');
-
-router.get('/get', testControllers.get);
-
-router.post('/create', testControllers.create);
-
-module.exports = router;
